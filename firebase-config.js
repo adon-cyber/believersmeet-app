12c7b56003c561b54abc2d759ea50c7c04b7a93b
adon-cyber <adoniakasango@gmail.com>
2026-08-08 18:11:10 +0300

    Update firebase-config with valid API credentials

--- firebase-config.js
+++ firebase-config.js
@@ -7,12 +7,13 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebas
 
 // Replace with your actual Firebase project credentials
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBwEcqaYSOvfEqsrNebFY_3ef0CkmQ1zcg",
+  authDomain: "believersmeet-app.firebaseapp.com",
+  projectId: "believersmeet-app",
+  storageBucket: "believersmeet-app.firebasestorage.app",
+  messagingSenderId: "950754781573",
+  appId: "1:950754781573:web:e69112b5ace0dc44d8fcec",
+  measurementId: "G-4XFQBVJ68B"
 };
 
 // Initialize Firebase App
